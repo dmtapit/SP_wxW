1e84d6817b23e613e86a0522aad201e452401e51
Dean Tapit <dmtapit@gmail.com>
2018-04-16 15:44:27 -0700

    Created box sizers for the buttons on both upper and lower

--- SP_wxW/MainFrame.cpp
+++ SP_wxW/MainFrame.cpp
@@ -31,26 +31,41 @@ MainFrame::MainFrame(const wxString& title) : wxFrame(NULL, wxID_ANY, title, wxD
 	mainPanel = new wxPanel(this, wxID_ANY);
 	wxSizer * sizerUpper = new wxBoxSizer(wxVERTICAL);
 
+	wxSizer * sizerUpperControls = new wxBoxSizer(wxHORIZONTAL);
+
 	int style = wxBK_DEFAULT; // Style for a wxNotebook
 
 	driveBook = new DriverNotebook(mainPanel, DriveBook_MainBook,
 		wxDefaultPosition, wxDefaultSize, style, "Drive - MainBook");
 
+	wxStaticBox *box = new wxStaticBox(mainPanel, wxID_ANY, wxT("Buttons For Notebook"));
+	wxSizer *sizerUpperBtns = new wxStaticBoxSizer(box, wxVERTICAL);
+	wxButton * btn;
+	btn = new wxButton(mainPanel, wxID_ANY, wxT("Test 1"));
+	sizerUpperBtns->Add(btn);
+	btn = new wxButton(mainPanel, wxID_ANY, wxT("Test 2"));
+	sizerUpperBtns->Add(btn);
+
+	sizerUpperControls->Add(driveBook, 1, wxGROW | (wxALL & ~(wxTOP | wxBOTTOM)), 10);
+	sizerUpperControls->Add(sizerUpperBtns);
+
 	// Bottom portion ////////////////////////////////////////////////////////////////
-	wxSizer * sizerLower = new wxBoxSizer(wxVERTICAL);
+	//wxSizer * sizerLower = new wxBoxSizer(wxVERTICAL);
+
+	wxStaticBox *box2 = new wxStaticBox(mainPanel, wxID_ANY, wxT("Master List"));
+	wxSizer *sizerLowerBtns = new wxStaticBoxSizer(box2, wxVERTICAL);
 
 	wxBoxSizer * sizerBtns = new wxBoxSizer(wxHORIZONTAL);
-	wxButton * btn;
 
 	btn = new wxButton(mainPanel, DriveBook_QuitBtn, wxT("Exit"));
 	sizerBtns->Add(btn);
-	sizerLower->Add(sizerBtns, 0, wxALL | wxALIGN_RIGHT, 5);
+	sizerLowerBtns->Add(sizerBtns, 0, wxALL | wxALIGN_RIGHT, 5);
 
 	// Combine both upper and lower parts together (notice how everything is
-	// technically all in the sizerUpper, which in turn will be in the mainPanel)
-	sizerUpper->Add(driveBook, 1, wxGROW | (wxALL & ~(wxTOP | wxBOTTOM)), 10);
+	// technically all contents are in sizerUpper, which in turn will be in the mainPanel)
+	sizerUpper->Add(sizerUpperControls, 1, wxGROW | (wxALL & ~(wxTOP | wxBOTTOM)), 10);
 	sizerUpper->Add(0, 5, 0, wxGROW); // Add spaced between the halves
-	sizerUpper->Add(sizerLower, 0, wxGROW | (wxALL & ~wxTOP), 10);
+	sizerUpper->Add(sizerLowerBtns, 0, wxGROW | (wxALL & ~wxTOP), 10);
 
 	mainPanel->SetSizer(sizerUpper);
 

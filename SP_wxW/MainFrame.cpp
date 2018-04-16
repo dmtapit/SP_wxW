4dbd7ae621fe08c572c9bf0ccd3f0286290c91ea
Dean Tapit <dmtapit@gmail.com>
2018-04-15 23:38:33 -0700

    Add a main panel, two sizers, and an exit button (non-functional)

--- SP_wxW/MainFrame.cpp
+++ SP_wxW/MainFrame.cpp
@@ -11,18 +11,35 @@ MainFrame::MainFrame(const wxString& title) : wxFrame(NULL, wxID_ANY, title, wxD
 	
 	SetMenuBar(menuBar);
 	Connect(wxID_EXIT, wxEVT_COMMAND_MENU_SELECTED, wxCommandEventHandler(MainFrame::OnQuit));
-	Centre();
 
-	// Panel which will contain the two halves: upper, lower
-
+	// Panel which will contain the two halves: upper, lower /////////////////////////
 	mainPanel = new wxPanel(this, wxID_ANY);
-	wxSizer * sizerUpper = new wxBoxSizer(wxHORIZONTAL);
+	wxSizer * sizerUpper = new wxBoxSizer(wxVERTICAL);
 
+	int style = wxBK_DEFAULT; // Style for a wxNotebook
 
+	driveBook = new DriverNotebook(mainPanel, DriveBook_MainBook,
+		wxDefaultPosition, wxDefaultSize, style, "Drive - MainBook");
+
+	// Bottom portion
 	wxSizer * sizerLower = new wxBoxSizer(wxVERTICAL);
 
+	wxBoxSizer * sizerBtns = new wxBoxSizer(wxHORIZONTAL);
+	wxButton * btn;
 
+	btn = new wxButton(mainPanel, DriveBook_QuitBtn, wxT("Exit"));
+	sizerBtns->Add(btn);
+	sizerLower->Add(sizerBtns, 0, wxALL | wxALIGN_RIGHT, 5);
 
+	// Combine both upper and lower parts together (notice how everything is
+	// technically all in the sizerUpper, which in turn will be in the mainPanel)
+	sizerUpper->Add(driveBook, 1, wxGROW | (wxALL & ~(wxTOP | wxBOTTOM)), 10);
+	sizerUpper->Add(0, 5, 0, wxGROW); // Add spaced between the halves
+	sizerUpper->Add(sizerLower, 0, wxGROW | (wxALL & ~wxTOP), 10);
+
+	mainPanel->SetSizer(sizerUpper);
+
+	Centre(); // Still not sure the difference between Centre() and Center(), if any...
 }
 
 void MainFrame::OnQuit(wxCommandEvent& WXUNUSED(event))
--- SP_wxW/MainFrame.h
+++ SP_wxW/MainFrame.h
@@ -1,5 +1,15 @@
 #pragma once
 #include <wx/wx.h>
+#include <wx/notebook.h>
+
+#define DriverNotebook wxNotebook
+
+enum 
+{
+	// Control ID's ///////////////////////////////////////////////////////
+	DriveBook_MainBook = wxID_HIGHEST + 1, // ID for the main book in upper panel
+	DriveBook_QuitBtn // Quit button in lower panel
+};
 
 class MainFrame : public wxFrame
 {
@@ -14,4 +24,7 @@ public:
 private:
 	// The panel which holds all the contents in the main frame
 	wxPanel * mainPanel;
+
+	// This is a notebook which should contain pages of a driver's schedule
+	DriverNotebook * driveBook;
 };
